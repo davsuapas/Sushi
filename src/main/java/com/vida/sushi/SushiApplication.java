ef1df1b8b92e123c9f0949a1b8766277dcd050cb
David Suarez Pascual <dav.sua.pas@gmail.com>
2016-12-08 20:10:57 +0100

    Initialize

---
+++ src/main/java/com/vida/sushi/SushiApplication.java
@@ -0,0 +1,12 @@
+package com.vida.sushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiApplication.class, args);
+	}
+}
